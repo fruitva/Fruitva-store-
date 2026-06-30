91bebc4992bc4fd92031311c8790a5f178c87431
fruitva <ba3409398@gmail.com>
2026-06-30 14:25:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Fruitva Website Loaded");
+const launchDate = new Date("December 1, 2026 00:00:00").getTime();
+
+setInterval(() => {
+
+const now = new Date().getTime();
+
+const distance = launchDate - now;
+
+const days = Math.floor(distance / (1000*60*60*24));
+
+const hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+
+const minutes = Math.floor((distance%(1000*60*60))/(1000*60));
+
+const seconds = Math.floor((distance%(1000*60))/1000);
+
+document.getElementById("days").innerHTML=days;
+
+document.getElementById("hours").innerHTML=hours;
+
+document.getElementById("minutes").innerHTML=minutes;
+
+document.getElementById("seconds").innerHTML=seconds;
+
+},1000);
